a1c4df3750aab56b9f09e43b719c328e794b5f3e
nickzylstra <nick.zylstra@gmail.com>
2019-11-07 12:29:47 -0800

    Starts refactor to fetch

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,22 +1,53 @@
-var searchYouTube = ({key, query, max = 5}, callback) => {
-  $.get('https://www.googleapis.com/youtube/v3/search', {
-    part: 'snippet',
+// var searchYouTube = ({key, query, max = 5}, callback) => {
+//   $.get('https://www.googleapis.com/youtube/v3/search', {
+//     part: 'snippet',
+//     key: key,
+//     q: query,
+//     maxResults: max,
+//     type: 'video',
+//     videoEmbeddable: 'true'
+//   })
+//     .done(({items}) => {
+//       if (callback) {
+//         callback(items);
+//       }
+//     })
+//     .fail(({responseJSON}) => {
+//       responseJSON.error.errors.forEach((err) =>
+//         console.error(err)
+//       );
+//     });
+// };
+
+var searchYouTube = function searchYouTube({key, query, max}, callback) {
+  let url = 'https://www.googleapis.com/youtube/v3/search?part=snippet';
+  let options = {
     key: key,
     q: query,
-    maxResults: max,
+    maxResults: max || 5,
     type: 'video',
     videoEmbeddable: 'true'
-  })
-    .done(({items}) => {
+  };
+  _.forEach(options, (propVal, propKey) => {
+    url += '&' + propKey + '=' + propVal;
+  });
+
+  const response = fetch(url);
+
+  response.then(
+    (({items}) => {
       if (callback) {
         callback(items);
       }
-    })
-    .fail(({responseJSON}) => {
+    }),
+    (({responseJSON}) => {
       responseJSON.error.errors.forEach((err) =>
         console.error(err)
       );
-    });
+    })
+  )
+
+  return response;
 };
 
 export default searchYouTube;
