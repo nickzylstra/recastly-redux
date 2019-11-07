dcc451a31417718671fdd92980e63910b6b80b62
nickzylstra <nick.zylstra@gmail.com>
2019-11-06 16:01:26 -0800

    Passes store tests

--- src/store/store.js
+++ src/store/store.js
@@ -3,4 +3,22 @@ import thunk from 'redux-thunk';
 import rootReducer from './../reducers/main.js';
 import exampleVideoData from '../data/exampleVideoData.js';
 
+/* var DummyState = {
+  videos: exampleVideoData,
+  currentVideo: exampleVideoData[0]
+}
+
+configureStore(DummyState) {} */
+
 // TODO:  Create your redux store, apply thunk as a middleware, and export it!
+const configureStore = function configureStore(initialState) {
+  return createStore(
+    rootReducer,
+    initialState,
+    applyMiddleware(thunk)
+  );
+};
+
+const store = configureStore();
+
+export default store;
